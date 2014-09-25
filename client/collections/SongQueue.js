df42829a491a993d44cfca3497593bc97f5637ae
Josh Lankford <lankford.josh@gmail.com>
2014-09-24 18:30:17 -0700

    Some stuff works

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -1,7 +1,12 @@
 // SongQueue.js - Defines a backbone model class for the song queue.
 var SongQueue = Songs.extend({
 
-  initialize: function(){
-  }
+initialize: function(){
+  this.on('add', function(song){
+    }, this);
+  },
 
+  enqueue: function(song){
+    this.add(song);
+  }
 });
--- client/views/SongQueueView.js
+++ client/views/SongQueueView.js
@@ -1,10 +1,14 @@
 // SongQueueView.js - Defines a backbone view class for the song queue.
 var SongQueueView = Backbone.View.extend({
 
+  tagName: "table",
+
   initialize: function() {
+  	this.render();
   },
 
   render: function() {
+  	this.$el.html('<th>Song Queue</th>');
     return this.$el;
   }
 
